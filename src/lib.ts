b4f6eae24b3d4847b25d870438e66975cde127c6
CraicOverflow89 <craic.overflow@hotmail.com>
2020-04-17 16:55:19 +0100

    Updated with latest.

--- src/lib.ts
+++ src/lib.ts
@@ -1,9 +1,39 @@
 /**
+ * Number Extension
+ */
+declare interface Number {
+	isInteger(): boolean
+	toPaddedString(count: number): string
+}
+
+/**
+ * Determines if a number is an integer
+ *
+ * @returns boolean
+ */
+Number.prototype.isInteger = function(): boolean {
+	return typeof this === "number" && isFinite(this) && Math.floor(this) === this
+}
+
+/**
+ * Creates a string with padded zeroes
+ *
+ * @param count The minimum number of digits
+ * @returns string
+ */
+Number.prototype.toPaddedString = function(count: number): string {
+	let result = this.toString()
+	result = "0".repeat(count - result.length) + result
+	return result
+}
+
+/**
  * Object Extension
  */
 declare interface Object {
 	apply(logic: (it: Object) => void): Object
 	let(logic: (it: Object) => any): any
+	when(logic: {}, none: () => void | null): void
 }
 
 /**
@@ -28,6 +58,116 @@ Object.prototype.let = function(logic: (it: Object) => any): any {
 }
 
 /**
+ * Invokes logic of matching condition
+ *
+ * @param logic Map of condition / result pairs
+ * @param none Logic to invoke if no condition is met
+ * @returns void
+ */
+Object.prototype.when = function(logic: {}, none: () => void = null): void {
+	// NOTE: need to be more explicit about logic type (keys are any and values are CallableFunction)
+	const condition = Object.keys(logic)
+	for(let x = 0; x < condition.length; x ++) {
+		if(condition[x] == this) {
+			if(typeof logic[condition[x]] == "function") logic[condition[x]]()
+			// NOTE: shouldn't have to perform this type check
+			return
+		}
+	}
+	if(none !== null) none()
+}
+
+/**
+ * String Extension
+ */
+declare interface String {
+	endsWith(value: string): boolean
+	repeat(count: number): string
+	startsWith(value: string): boolean
+}
+
+/**
+ * Determines if a string ends with a string
+ *
+ * @param value The string to check
+ * @returns boolean
+ */
+String.prototype.endsWith = function(value: string): boolean {
+	return this.substr(this.length - value.length) == value
+}
+
+/**
+ * Repeats a string a number of times
+ *
+ * @param count The amount of times to repeat
+ * @returns string
+ */
+String.prototype.repeat = function(count: number): string {
+	if(count < 1 || !count.isInteger()) return ""
+	let result = this
+	let x = 0
+	while(++ x < count) result += result
+	return result
+}
+
+/**
+ * Determines if a string starts with a string
+ *
+ * @param value The string to check
+ * @returns boolean
+ */
+String.prototype.startsWith = function(value: string): boolean {
+	return this.substr(0, value.length) == value
+}
+
+/**
+ * Bound Property Data
+ */
+class BoundProperty<T> {
+	getter: () => T
+	setter: (value: T) => void
+
+	constructor(getter: () => T, setter: (value: T) => void) {
+		this.getter = getter
+		this.setter = setter
+	}
+
+	/**
+	 * Creates a BoundProperty using an element and property
+	 *
+	 * @param element The element to bind to
+	 * @param property The property to bind to
+	 * @return BoundProperty
+	 */
+	static bind<T1>(element: HTMLElement, property: string): BoundProperty<T1> {
+		return new BoundProperty<T1>(() => {
+			return element[property]
+		}, (value: T1) => {
+			element[property] = value
+		})
+	}
+
+	/**
+	 * Gets the value of the bound property
+	 *
+	 * @returns T
+	 */
+	get(): T {
+		return this.getter()
+	}
+
+	/**
+	 * Sets the value of the bound property
+	 *
+	 * @param value The new value to set
+	 * @returns void
+	 */
+	set(value: T): void {
+		this.setter(value)
+	}
+}
+
+/**
  * Colour Data
  */
 class Colour {
@@ -47,20 +187,99 @@ class Colour {
 	 * @returns string
 	 */
 	toHex(): string {
-		const toHex = function(value: number) {
+		return (function(value: number) {
 			return Number(value).toString(16).let((it: string) => {
 				if(it.length < 2) it = "0" + it
 				return it
 			})
-		}
-		return ("#" + toHex(this.r) + toHex(this.g) + toHex(this.b)).toUpperCase()
+		}).let((it: CallableFunction) => {
+			return ("#" + it(this.r) + it(this.g) + it(this.b)).toUpperCase()
+		})
 	}
 }
 
 /**
- * Point Data
+ * Map of Key / Value Entries
  */
-class Point {
+class HashMap<T> {
+	data: {} = {}
+
+	/**
+	 * Clears all data in the map
+	 *
+	 * @returns void
+	 */
+	clear(): void {
+		this.data = {}
+	}
+
+	/**
+	 * Checks if the map contains a key
+	 *
+	 * @param key The key to search for
+	 * @returns boolean
+	 */
+	containsKey(key: string): boolean {
+		return this.data.hasOwnProperty(key)
+	}
+
+	/**
+	 * Gets data from the map
+	 *
+	 * @param key The key to read from
+	 * @returns any
+	 */
+	get(key: string): T {
+		return this.data[key]
+	}
+
+	/**
+	 * Puts data in the map
+	 *
+	 * @param key The key to write with
+	 * @param value The value to write
+	 * @returns void
+	 */
+	put(key: string, value: T): void {
+		this.data[key] = value
+	}
+
+	/**
+	 * Removes an entry from the map
+	 *
+	 * @param key The key to search for
+	 * @returns boolean
+	 */
+	remove(key: string): boolean {
+
+		// Not Found
+		if(!this.data.hasOwnProperty(key)) return false
+
+		// Remove Entry
+		delete this.data[key]
+		return true
+	}
+
+	/**
+	 * Creates an array of data
+	 *
+	 * @param logic The logic to invoke against each entry
+	 * @returns Array
+	 */
+	toArray<R>(logic: (k: string, v: T) => R): Array<R> {
+		const result = []
+		const keys = Object.keys(this.data)
+		for(let x = 0; x < keys.length; x ++) {
+			result.push(logic(keys[x], this.data[keys[x]]))
+		}
+		return result
+	}
+}
+
+/**
+ * 2D Point Data
+ */
+class Point2D {
 	x: number
 	y: number
 
@@ -71,15 +290,41 @@ class Point {
 }
 
 /**
+ * 3D Point Data
+ */
+class Point3D {
+	x: number
+	y: number
+	z: number
+
+	constructor(x: number, y: number, z: number) {
+		this.x = x
+		this.y = y
+		this.z = z
+	}
+}
+
+/**
  * String Buffer
  */
 class StringBuffer {
 	data: Array<string> = []
 
+	/**
+	 * Appends data to the buffer
+	 *
+	 * @param data The data to append
+	 * @returns void
+	 */
 	append(data: string): void {
 		this.data.push(data)
 	}
 
+	/**
+	 * Creates a string from the buffer
+	 *
+	 * @returns string
+	 */
 	toString(): string {
 		return this.data.join("")
 	}
